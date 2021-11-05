40340a02b3172a4f2e5a846d18e3d7c68131209b
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-05 12:03:59 -0600

    12:03pm back-end draft

---
+++ client/main.js
@@ -0,0 +1,46 @@
+const userContainer = document.querySelector('#user-info')
+const registerForm = document.querySelector('#register-form')
+
+const baseURL = `http://localhost:4000/api`
+
+const register = body => axios.post(`${baseURL}/register`, body)
+    .then(res => {createUserCard(res.data)
+    }) .catch(err => {
+        console.log(err)
+        alert('Error, please try again.')
+    })
+    
+function registerSubmitHandler(e) {
+    e.preventDefault()
+        
+    let firstName = document.querySelector('#register-firstName')
+    let lastName = document.querySelector('#register-lastName')
+    let email = document.querySelector('#register-email')
+        
+    let bodyObj = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email
+        }
+    register(bodyObj)
+        
+    firstName.value = ''
+    lastName.value = ''
+    email = ''
+}
+
+function createUserCard(data) {
+    userContainer.innerHTML = ''
+    const userCard = document.createElement('div')
+    userCard.classList.add('user-card')
+
+    userCard.innerHTML = `<p class="email">Email: ${data.email}</p>
+    <p class="first-name">First Name: ${data.firstName}</p>
+    <p class="last-name">Last Name: ${data.lastName}</p>
+    `
+
+    userContainer.appendChild(userCard)
+}
+
+registerForm.addEventListener('submit', registerSubmitHandler)
+    
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt")
 
 const app = express();
 
@@ -21,5 +22,34 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+
+app.get("/api/fortune", (req, res) => {
+  const fortune = ["The truly generous share, even with the undeserving", 
+  "Savor your freedom, it is precious", 
+  "It's not the amount of time you devote, but what you devote to the time that counts.", 
+  "Your success will astonish everyone.",
+  "Each day, compel yourself to do something you would rather not do.",]
+
+
+let randomIndex = Math.floor(Math.random() * fortune.length)
+let randomFortune = fortune[randomIndex]
+
+res.status(200).send(randomFortune)
+
+})
+
+const users = []
+app.post("/api/register", (req, res) => {
+  console.log("Registering User")
+  const {firstName, lastName, email} = req.body
+  let newUser = {
+    firstName,
+    lastName,
+    email
+  }
+  users.push(newUser)
+  console.log(users)
+  res.status(200).send("Information secured, thank you for joining!")
+})
 
 app.listen(4000, () => console.log("Server running on 4000"));
