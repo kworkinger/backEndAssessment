6e9842ab7c81727e7fb97632735441770f2dd916
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-07 23:37:51 -0700

    Idk how to connect them all Jeddy! I spent 15 hours, this was a huge jump from lab exercises.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,10 @@
 const userContainer = document.querySelector('#user-info')
 const registerForm = document.querySelector('#register-form')
+const addGoal = document.querySelector('#add-goal')
+const goal = document.querySelector('#goal')
+const goalsContainer = document.querySelector('#goalList')
+const updateUsers = document.querySelector('#update')
+const deleteUsers = document.querySelector('#delete')
 
 const baseURL = `http://localhost:4000/api`
 
@@ -10,6 +15,22 @@ const register = body => axios.post(`${baseURL}/register`, body)
         alert('Error, please try again.')
     })
     
+const addGoals = body => axios.post(`${baseURL}/goals`, body)
+    .then(res => {createGoals(res.data)
+    })  .catch(err => {
+        console.log(err)
+        alert('Error transmitting goal.')
+    })
+
+const update = body => axios.put(`${baseURL}/${id}`, body)
+    .then(res => {createUserCard(res.data)
+    }) .catch(err => {
+        console.log(err)
+        alert('Error updating user.')
+    })
+
+const deleteUser = () => axios.put(`${baseURL}/${id}`)
+
 function registerSubmitHandler(e) {
     e.preventDefault()
         
@@ -29,6 +50,53 @@ function registerSubmitHandler(e) {
     email.value = ''
 }
 
+function addGoalHandler(e){
+    e.preventDefault()
+    
+    newGoal = {
+        goal: goal.value
+    }
+    addGoals(newGoal)
+
+    goal.value = ''
+}
+
+function updateUserHandler(e){
+    e.preventDefault()
+
+    let firstName = document.querySelector('#register-firstName')
+    let lastName = document.querySelector('#register-lastName')
+    let email = document.querySelector('#register-email')
+        
+    let bodyObj = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value
+        }
+    update(bodyObj)
+        
+    firstName.value = ''
+    lastName.value = ''
+    email.value = ''
+
+}
+
+function deleteUserHandler(e) {
+    e.preventDefault()
+    
+    let firstName = document.querySelector('#register-firstName')
+    let lastName = document.querySelector('#register-lastName')
+    let email = document.querySelector('#register-email')
+        
+    let bodyObj = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value
+        }
+
+    deleteUser(bodyObj)
+}
+
 function createUserCard(data) {
     userContainer.innerHTML = ''
     const userCard = document.createElement('div')
@@ -42,5 +110,21 @@ function createUserCard(data) {
     userContainer.appendChild(userCard)
 }
 
+function createGoals(data) {
+    goalsContainer.innerHTML = ''
+    // for(let i = 0; i < res.data; i++){
+    let postGoal = document.createElement('li')
+    postGoal.classList.add('post-goal')
+    // postGoal.textContent = res.data[i]
+    postGoal.innerHTML = `${data.goals}`
+
+    goalsContainer.appendChild(postGoal)
+
+}
+// }
+
+
 registerForm.addEventListener('submit', registerSubmitHandler)
-    
+addGoal.addEventListener('click', addGoalHandler)    
+updateUsers.addEventListener('click', updateUserHandler)
+deleteUsers.addEventListener('click', deleteUserHandler)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { response } = require("express");
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
+  return
 });
 
 app.get("/api/fortune", (req, res) => {
@@ -35,14 +36,15 @@ let randomIndex = Math.floor(Math.random() * fortune.length)
 let randomFortune = fortune[randomIndex]
 
 res.status(200).send(randomFortune)
-
+return
 })
 
 const users = []
-const globalId = 1
+let globalId = 1
 app.post("/api/register", (req, res) => {
   console.log("Registering User")
-  const {firstName, lastName, email} = req.body
+  // console.log(req.body)
+  let {firstName, lastName, email} = req.body
   let newUser = {
     id: globalId,
     firstName,
@@ -53,17 +55,84 @@ app.post("/api/register", (req, res) => {
   console.log(newUser)
   console.log(users)
   res.status(200).send(newUser)
+  
   globalId++
+  return
+})
+
+let goals = []
+let count = 0
+
+app.post("/api/goals", (req, res) => {
+  console.log("Creating goals")
+  // console.log(req.body)
+  //returning an object with key: value of goal: 'run'
+  const {goal} = req.body
+  newGoal = {
+    id: count,
+    goal: goal}
+  goals.push(newGoal)
+  res.status(200).send(newGoal)
+  console.log(newGoal)
+  console.log(goals)
+  count++
+  return
+})
+
+// app.put("api/register/:id", (req, res) => {
+//   let existingid = req.params.id
+//   let index = users.findIndex(elem => elem.id === id)
+//   for(let i = 0; i < users.length; i++) {
+//     if(users[i].firstName === existing){
+//       users[index].firstName = req.body.firstName
+//       users[index].lastName = req.body.lastName
+//       users[index].email = req.body.email
+//       res.status(200).send("User updated.")
+//       return
+//     } else if (users[i].lastName === existingUserInfo) {
+//       users[i].firstName = req.body.firstName
+//       users[i].lastName = req.body.lastName
+//       users[i].email = req.body.email
+//       res.status(200).send("User updated.")
+//       return
+//     } else if (users[i].email === existingUserInfo) {
+//       users[i] = newUserInfo
+//       res.status(200).send("User updated")
+//       return
+//     }
+//   }
+//   res.status(400).send("User not found.")
+// })
+app.put("/api/register/:id", (req, res) => {
+  console.log("Changing User Info")
+  let {id} = req.params
+  let {firstName, lastName, email} = req.body
+  let index = users.findIndex(elem => elem.id === +id)
+
+  if (users[index].firstName === id) {
+    users[index].lastName = lastName
+    users[index].email = email
+    res.status(200).send("User updated")
+  } else if (users[index].lastName === id) {
+    users[index].firstName = firstName
+    users[index].email = email
+    // res.status(200).send("User updated")
+  } else if (users[index].email === id) {
+    users[index].firstName = firstName
+    users[index].lastName = lastName
+    // res.status(200).send("User updated")
+  } else {
+    res.status(400).send("error")
+    return
+  }
+  res.status(200).send(users[index])
+  return
+})
+
+app.delete("/api/register/:id", (req, res) => {
+  let index = users.findIndex(elem => elem.id === +req.params.id)
+  users.splice(index, 1)
+  res.status(200).send("User deleted.")
 })
 
 app.listen(4000, () => console.log("Server running on 4000"));
-
-app.put("/api/register/:id", (req, res) => {
-  console.log("Changing User Info")
-  let {id} = request.params
-  let index = users.findIndex(elem => elem.id === +id)
-  const {firstName, lastName, email} = req.body
-  
-  
-  
-})
